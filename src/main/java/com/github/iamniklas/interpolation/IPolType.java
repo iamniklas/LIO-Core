6d00d4e748cb003e135cfcc5ff23ba4ce833cee2
iamniklas <niklas.englmeier@web.de>
2021-10-28 22:28:40 +0200

    IPolType

---
+++ src/main/java/com/github/iamniklas/interpolation/IPolType.java
@@ -0,0 +1,15 @@
+package com.github.iamniklas.interpolation;
+
+public enum IPolType {
+    Linear,
+    EaseInSine, EaseOutSine, EaseInOutSine,
+    EaseInQuad, EaseOutQuad, EaseInOutQuad,
+    EaseInCubic, EaseOutCubic, EaseInOutCubic,
+    EaseInQuart, EaseOutQuart, EaseInOutQuart,
+    EaseInQuint, EaseOutQuint, EaseInOutQuint,
+    EaseInExpo, EaseOutExpo, EaseInOutExpo,
+    EaseInCirc, EaseOutCirc, EaseInOutCirc,
+    EaseInBack, EaseOutBack, EaseInOutBack,
+    EaseInElastic, EaseOutElastic, EaseInOutElastic,
+    EaseInBounce, EaseOutBounce, EaseInOutBounce
+}
