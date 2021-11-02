8ef86366c0b46621646507db61c145d49da834c5
iamniklas <niklas.englmeier@web.de>
2021-11-02 17:44:28 +0100

    Rainbow(Mono) procedures

--- src/main/java/com/github/iamniklas/liocore/procedures/variants/RainbowMonoProcedure.java
+++ src/main/java/com/github/iamniklas/liocore/procedures/variants/RainbowMonoProcedure.java
@@ -1,24 +1,32 @@
 package com.github.iamniklas.liocore.procedures.variants;
 
 import com.github.iamniklas.liocore.led.LEDDataBundle;
+import com.github.iamniklas.liocore.led.colorspace.ColorHSV;
 import com.github.iamniklas.liocore.procedures.Procedure;
 
 public class RainbowMonoProcedure extends Procedure {
 
     private LEDDataBundle bundle;
 
+    private ColorHSV colorHsv = new ColorHSV(0, 1.0f, 1.0f);
+    private float speed = 1.0f;
+
     public RainbowMonoProcedure(LEDDataBundle _bundle) {
         super(_bundle);
         bundle = _bundle;
+
+        speed = Math.max(1.0f, bundle.speed);
     }
 
     @Override
     public void start() {
-
+        System.out.println("RainbowMono Start");
     }
 
     @Override
     public void update() {
-
+        System.out.println("RainbowMono Update");
+        colorHsv.h = colorHsv.h > 360.0f ? 0 : (int) (colorHsv.h + speed);
+        strip.setAllPixels(colorHsv.toRGB().toSystemColor());
     }
 }
--- src/main/java/com/github/iamniklas/liocore/procedures/variants/RainbowProcedure.java
+++ src/main/java/com/github/iamniklas/liocore/procedures/variants/RainbowProcedure.java
@@ -1,21 +1,38 @@
 package com.github.iamniklas.liocore.procedures.variants;
 
 import com.github.iamniklas.liocore.led.LEDDataBundle;
+import com.github.iamniklas.liocore.led.LEDStripManager;
+import com.github.iamniklas.liocore.led.colorspace.ColorHSV;
 import com.github.iamniklas.liocore.procedures.Procedure;
+import com.github.iamniklas.liocore.procedures.models.Direction;
 
 public class RainbowProcedure extends Procedure {
 
     private LEDDataBundle bundle;
 
+    private ColorHSV colorHSV = new ColorHSV(0, 1.0f, 1.0f);
+    private float hueCounter = 0.0f;
+    private float[] hueArrayCounter = new float[LEDStripManager.LED_COUNT];
+
+    private float repetitions = 0.75f;
+    private float speed = 3.0f;
+    private Direction direction = Direction.Left;
+
     public RainbowProcedure(LEDDataBundle _bundle) {
         super(_bundle);
         bundle = _bundle;
+
+        repetitions = bundle.repetitions;
+        speed = bundle.speed;
+        direction = bundle.direction;
+
+        for (int i = 0; i < hueArrayCounter.length; i++) {
+            hueArrayCounter[i] = Math.abs(i - 150);
+        }
     }
 
     @Override
-    public void start() {
-
-    }
+    public void start() { }
 
     @Override
     public void update() {
