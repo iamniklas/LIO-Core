438fc8c9f08c1944d72eda396b2eac9588f17ab2
iamniklas <niklas.englmeier@web.de>
2021-12-15 15:31:23 +0100

    Minor MQTTListener changes

--- src/main/java/com/github/iamniklas/liocore/network/mqtt/MQTTPublisher.java
+++ src/main/java/com/github/iamniklas/liocore/network/mqtt/MQTTPublisher.java
@@ -4,13 +4,15 @@ import org.eclipse.paho.client.mqttv3.IMqttClient;
 import org.eclipse.paho.client.mqttv3.MqttClient;
 import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
 
 import java.util.UUID;
 import java.util.concurrent.Callable;
 
 public class MQTTPublisher {
 
-    public IMqttClient client = new MqttClient("tcp://000raspberry.ddns.net:1883", UUID.randomUUID().toString());
+    private final MemoryPersistence persistence = new MemoryPersistence();
+    public IMqttClient client = new MqttClient("tcp://000raspberry.ddns.net:1883", UUID.randomUUID().toString(), persistence);
 
     public MQTTPublisher() throws MqttException {
         MqttConnectOptions options = new MqttConnectOptions();
